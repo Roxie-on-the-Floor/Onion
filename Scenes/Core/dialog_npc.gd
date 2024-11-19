cc063132fae82eda4b343b16232f77ac0c5a5d49
Sammy Sherifi <sammysherifi@gmail.com>
2024-11-19 09:49:42 -0500

    Beginning dialog system

--- Scenes/Core/dialog_npc.gd
+++ Scenes/Core/dialog_npc.gd
@@ -19,17 +19,18 @@ func _ready() -> void:
 
 
 func _interaction():
+		pass
 		# Connect the method we want the "DialogManager Server" to trigger when we end the event
-		DialogManager.dialogue_complete.connect(interact_component._event_end)
+		#DialogManager.dialogue_complete.connect(interact_component._event_end)
 		
 		# We have some setup to do before we can start the dialog, we will get the textbox position
 		# TO FIX this could probably be moved to the dialogmanager script
-		var text_box_position = camera_3d.unproject_position(global_position)
+		#var text_box_position = camera_3d.unproject_position(global_position)
 		# Check which flag we are on, and then start the corresponding dialogue, we work in a "queue" type
 		# deal and we stay on the last said dialog. 
 		
 		#DialogManager.start_dialog(text_box_position, lines, mouth_sprite, pitch)
 		# Await the signal
-		await DialogManager.dialogue_complete
+		#await DialogManager.dialogue_complete
 		# Disconnect when we are done
-		DialogManager.dialogue_complete.disconnect(interact_component._event_end)
+		#DialogManager.dialogue_complete.disconnect(interact_component._event_end)
